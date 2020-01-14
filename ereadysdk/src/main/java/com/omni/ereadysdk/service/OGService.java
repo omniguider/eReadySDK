cfce6f943dce0866a99b11d7e1470199054bea50
ok0225 <leo.hsiao@omniguider.com>
2020-01-14 14:57:07 +0800

    build issue fix

--- ereadysdk/src/main/java/com/omni/ereadysdk/service/OGService.java
+++ ereadysdk/src/main/java/com/omni/ereadysdk/service/OGService.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -23,8 +22,6 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.Objects;
-
-import static com.omni.ereadysdk.tool.eReadyText.LOG_TAG;
 
 public class OGService implements LocationListener,
         GoogleApiClient.ConnectionCallbacks,
